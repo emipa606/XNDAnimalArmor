76d0b43363421fdafbac619bc7b8058488add00c
Mlie <emipa606@users.noreply.github.com>
2022-10-11 21:15:33 +0200

    Mod updated for 1.4 and passed autotests

--- Source/AnimalArmourCrowbar/AnimalArmourFix.cs
+++ Source/AnimalArmourCrowbar/AnimalArmourFix.cs
@@ -1,79 +1,78 @@
 ﻿using System.Linq;
 using Verse;
 
-namespace AnimalArmourFix
-{
-    [StaticConstructorOnStartup]
-    public static class AnimalArmourFix
-    {
-        static AnimalArmourFix()
-        {
-            var animalsInGame = (from animal in DefDatabase<ThingDef>.AllDefsListForReading
-                where animal.race is {Animal: true}
-                select animal).ToList();
-            foreach (var animalDef in animalsInGame)
-            {
-                switch (animalDef.race.baseBodySize)
-                {
-                    case >= 2.4f:
-                        animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("InstallMakeshiftAnimalArmorHuge",
-                            false));
-                        animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("InstallStandardAnimalArmorHuge", false));
-                        animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("InstallHeavyAnimalArmorHuge", false));
-                        animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("RemoveMakeshiftAnimalArmorHuge", false));
-                        animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("RemoveStandardAnimalArmorHuge", false));
-                        animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("RemoveHeavyAnimalArmorHuge", false));
-                        break;
-                    case >= 1.3f:
-                        animalDef.recipes.Add(
-                            DefDatabase<RecipeDef>.GetNamed("InstallMakeshiftAnimalArmorLarge", false));
-                        animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("InstallStandardAnimalArmorLarge",
-                            false));
-                        animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("InstallHeavyAnimalArmorLarge", false));
-                        animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("RemoveMakeshiftAnimalArmorLarge",
-                            false));
-                        animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("RemoveStandardAnimalArmorLarge", false));
-                        animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("RemoveHeavyAnimalArmorLarge", false));
-                        break;
-                    case >= 0.7f:
-                        animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("InstallMakeshiftAnimalArmorMedium",
-                            false));
-                        animalDef.recipes.Add(
-                            DefDatabase<RecipeDef>.GetNamed("InstallStandardAnimalArmorMedium", false));
-                        animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("InstallHeavyAnimalArmorMedium", false));
-                        animalDef.recipes.Add(
-                            DefDatabase<RecipeDef>.GetNamed("RemoveMakeshiftAnimalArmorMedium", false));
-                        animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("RemoveStandardAnimalArmorMedium",
-                            false));
-                        animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("RemoveHeavyAnimalArmorMedium", false));
-                        break;
-                    case >= 0.3f:
-                        animalDef.recipes.Add(
-                            DefDatabase<RecipeDef>.GetNamed("InstallMakeshiftAnimalArmorSmall", false));
-                        animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("InstallStandardAnimalArmorSmall",
-                            false));
-                        animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("InstallHeavyAnimalArmorSmall", false));
-                        animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("RemoveMakeshiftAnimalArmorSmall",
-                            false));
-                        animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("RemoveStandardAnimalArmorSmall", false));
-                        animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("RemoveHeavyAnimalArmorSmall", false));
-                        break;
-                    default:
-                        animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("InstallMakeshiftAnimalArmorTiny",
-                            false));
-                        animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("InstallStandardAnimalArmorTiny", false));
-                        animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("InstallHeavyAnimalArmorTiny", false));
-                        animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("RemoveMakeshiftAnimalArmorTiny", false));
-                        animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("RemoveStandardAnimalArmorTiny", false));
-                        animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("RemoveHeavyAnimalArmorTiny", false));
-                        break;
-                }
-            }
+namespace AnimalArmourFix;
 
-            if (Prefs.DevMode)
+[StaticConstructorOnStartup]
+public static class AnimalArmourFix
+{
+    static AnimalArmourFix()
+    {
+        var animalsInGame = (from animal in DefDatabase<ThingDef>.AllDefsListForReading
+            where animal.race is { Animal: true }
+            select animal).ToList();
+        foreach (var animalDef in animalsInGame)
+        {
+            switch (animalDef.race.baseBodySize)
             {
-                Log.Message($"XND Animal Armour: Added armour recipies to {animalsInGame.Count} animal-spieces");
+                case >= 2.4f:
+                    animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("InstallMakeshiftAnimalArmorHuge",
+                        false));
+                    animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("InstallStandardAnimalArmorHuge", false));
+                    animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("InstallHeavyAnimalArmorHuge", false));
+                    animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("RemoveMakeshiftAnimalArmorHuge", false));
+                    animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("RemoveStandardAnimalArmorHuge", false));
+                    animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("RemoveHeavyAnimalArmorHuge", false));
+                    break;
+                case >= 1.3f:
+                    animalDef.recipes.Add(
+                        DefDatabase<RecipeDef>.GetNamed("InstallMakeshiftAnimalArmorLarge", false));
+                    animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("InstallStandardAnimalArmorLarge",
+                        false));
+                    animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("InstallHeavyAnimalArmorLarge", false));
+                    animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("RemoveMakeshiftAnimalArmorLarge",
+                        false));
+                    animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("RemoveStandardAnimalArmorLarge", false));
+                    animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("RemoveHeavyAnimalArmorLarge", false));
+                    break;
+                case >= 0.7f:
+                    animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("InstallMakeshiftAnimalArmorMedium",
+                        false));
+                    animalDef.recipes.Add(
+                        DefDatabase<RecipeDef>.GetNamed("InstallStandardAnimalArmorMedium", false));
+                    animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("InstallHeavyAnimalArmorMedium", false));
+                    animalDef.recipes.Add(
+                        DefDatabase<RecipeDef>.GetNamed("RemoveMakeshiftAnimalArmorMedium", false));
+                    animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("RemoveStandardAnimalArmorMedium",
+                        false));
+                    animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("RemoveHeavyAnimalArmorMedium", false));
+                    break;
+                case >= 0.3f:
+                    animalDef.recipes.Add(
+                        DefDatabase<RecipeDef>.GetNamed("InstallMakeshiftAnimalArmorSmall", false));
+                    animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("InstallStandardAnimalArmorSmall",
+                        false));
+                    animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("InstallHeavyAnimalArmorSmall", false));
+                    animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("RemoveMakeshiftAnimalArmorSmall",
+                        false));
+                    animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("RemoveStandardAnimalArmorSmall", false));
+                    animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("RemoveHeavyAnimalArmorSmall", false));
+                    break;
+                default:
+                    animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("InstallMakeshiftAnimalArmorTiny",
+                        false));
+                    animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("InstallStandardAnimalArmorTiny", false));
+                    animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("InstallHeavyAnimalArmorTiny", false));
+                    animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("RemoveMakeshiftAnimalArmorTiny", false));
+                    animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("RemoveStandardAnimalArmorTiny", false));
+                    animalDef.recipes.Add(DefDatabase<RecipeDef>.GetNamed("RemoveHeavyAnimalArmorTiny", false));
+                    break;
             }
+        }
+
+        if (Prefs.DevMode)
+        {
+            Log.Message($"XND Animal Armour: Added armour recipies to {animalsInGame.Count} animal-spieces");
         }
     }
 }
--- Source/AnimalArmourCrowbar/HarmonyPatches.cs
+++ Source/AnimalArmourCrowbar/HarmonyPatches.cs
@@ -1,70 +1,64 @@
-﻿using HarmonyLib;
+using HarmonyLib;
 using RimWorld;
 using Verse;
 
-namespace AnimalArmourFix
+namespace AnimalArmourFix;
+
+[StaticConstructorOnStartup]
+internal static class HarmonyPatches
 {
-    // Token: 0x02000002 RID: 2
-    [StaticConstructorOnStartup]
-    internal static class HarmonyPatches
+    static HarmonyPatches()
     {
-        // Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
-        static HarmonyPatches()
+        var harmonyInstance = new Harmony("mehni.rimworld.animalarmour.main");
+        harmonyInstance.Patch(AccessTools.Method(typeof(Recipe_RemoveHediff), "ApplyOnPawn"),
+            new HarmonyMethod(typeof(HarmonyPatches), "RemoveHediff_Prefix"));
+        harmonyInstance.Patch(AccessTools.Method(typeof(Recipe_RemoveHediff), "ApplyOnPawn"), null,
+            new HarmonyMethod(typeof(HarmonyPatches), "RemoveHediff_Postfix"));
+        harmonyInstance.Patch(AccessTools.Method(typeof(Recipe_InstallImplant), "ApplyOnPawn"),
+            new HarmonyMethod(typeof(HarmonyPatches), "SpawnHediff_Prefix"));
+    }
+
+    private static void RemoveHediff_Prefix(Recipe_RemoveHediff __instance, ref Pawn pawn, ref BodyPartRecord part,
+        out Hediff __state)
+    {
+        var currentPart = part;
+        var hediff = pawn.health.hediffSet.hediffs.Find(x =>
+            x.def == __instance.recipe.removesHediff && x.Part == currentPart && x.Visible);
+        __state = hediff;
+    }
+
+    private static void RemoveHediff_Postfix(ref Pawn billDoer, Hediff __state)
+    {
+        Log.Message(__state.def.defName);
+        if (__state.def.defName.Contains("AnimalArmor") &&
+            __state.def.spawnThingOnRemoved != null)
         {
-            var harmonyInstance = new Harmony("mehni.rimworld.animalarmour.main");
-            harmonyInstance.Patch(AccessTools.Method(typeof(Recipe_RemoveHediff), "ApplyOnPawn"),
-                new HarmonyMethod(typeof(HarmonyPatches), "RemoveHediff_Prefix"));
-            harmonyInstance.Patch(AccessTools.Method(typeof(Recipe_RemoveHediff), "ApplyOnPawn"), null,
-                new HarmonyMethod(typeof(HarmonyPatches), "RemoveHediff_Postfix"));
-            harmonyInstance.Patch(AccessTools.Method(typeof(Recipe_InstallImplant), "ApplyOnPawn"),
-                new HarmonyMethod(typeof(HarmonyPatches), "SpawnHediff_Prefix"));
+            GenSpawn.Spawn(__state.def.spawnThingOnRemoved, billDoer.Position, billDoer.Map);
+        }
+    }
+
+    private static void SpawnHediff_Prefix(Recipe_InstallImplant __instance, ref Pawn pawn, ref BodyPartRecord part,
+        ref Pawn billDoer)
+    {
+        if (pawn.RaceProps.Humanlike || __instance.recipe.addsHediff == null ||
+            !__instance.recipe.addsHediff.defName.Contains("AnimalArmor"))
+        {
+            return;
         }
 
-        // Token: 0x06000002 RID: 2 RVA: 0x00002100 File Offset: 0x00000300
-        private static void RemoveHediff_Prefix(Recipe_RemoveHediff __instance, ref Pawn pawn, ref BodyPartRecord part,
-            out Hediff __state)
+        var hediffs = pawn.health.hediffSet.hediffs;
+        for (var i = hediffs.Count - 1; i >= 0; i--)
         {
-            var currentPart = part;
-            var hediff = pawn.health.hediffSet.hediffs.Find(x =>
-                x.def == __instance.recipe.removesHediff && x.Part == currentPart && x.Visible);
-            __state = hediff;
-        }
-
-        // Token: 0x06000003 RID: 3 RVA: 0x00002148 File Offset: 0x00000348
-        private static void RemoveHediff_Postfix(ref Pawn billDoer, Hediff __state)
-        {
-            Log.Message(__state.def.defName);
-            if (__state.def.defName.Contains("AnimalArmor") &&
-                __state.def.spawnThingOnRemoved != null)
+            var hediff = hediffs[i];
+            if (hediff.Part != part || !hediff.def.defName.Contains("AnimalArmor") ||
+                hediff.def.defName == __instance.recipe.addsHediff.defName)
             {
-                GenSpawn.Spawn(__state.def.spawnThingOnRemoved, billDoer.Position, billDoer.Map);
-            }
-        }
-
-        // Token: 0x06000004 RID: 4 RVA: 0x000021B0 File Offset: 0x000003B0
-        private static void SpawnHediff_Prefix(Recipe_InstallImplant __instance, ref Pawn pawn, ref BodyPartRecord part,
-            ref Pawn billDoer)
-        {
-            if (pawn.RaceProps.Humanlike || __instance.recipe.addsHediff == null ||
-                !__instance.recipe.addsHediff.defName.Contains("AnimalArmor"))
-            {
-                return;
+                continue;
             }
 
-            var hediffs = pawn.health.hediffSet.hediffs;
-            for (var i = hediffs.Count - 1; i >= 0; i--)
-            {
-                var hediff = hediffs[i];
-                if (hediff.Part != part || !hediff.def.defName.Contains("AnimalArmor") ||
-                    hediff.def.defName == __instance.recipe.addsHediff.defName)
-                {
-                    continue;
-                }
-
-                var hediff2 = hediffs[i];
-                pawn.health.RemoveHediff(hediff2);
-                GenSpawn.Spawn(hediff2.def.spawnThingOnRemoved, billDoer.Position, billDoer.Map);
-            }
+            var hediff2 = hediffs[i];
+            pawn.health.RemoveHediff(hediff2);
+            GenSpawn.Spawn(hediff2.def.spawnThingOnRemoved, billDoer.Position, billDoer.Map);
         }
     }
 }
